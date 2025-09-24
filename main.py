7ddca50c7bd5e50754a147adda31113da7a9b803
Karma <ledombre@gmail.com>
2025-09-24 23:41:38 +0200

    Update bot status to do not disturb when watching for game server requests

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ class AMPDiscordBot(commands.Bot):
         activity = discord.Activity(
             type=discord.ActivityType.watching, name="for game server requests"
         )
-        await self.change_presence(activity=activity, status=discord.Status.online)
+        await self.change_presence(activity=activity, status=discord.Status.do_not_disturb)
 
     async def on_error(self, event, *args, **kwargs):
         """Handle bot errors."""
